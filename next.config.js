739549701670ca615daab39a1c400e090819a0d2
NamTH <namthit98@gmail.com>
2022-02-16 16:32:55 +0700

    fix bug build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // future: {
+  //   webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
+  //     // Looks like backward compatibility approach.
+  // },
   reactStrictMode: true,
-  images: {
-    domains: ["admin.kukiteddy.com", "kukiteddy.com"],
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
   },
 };
